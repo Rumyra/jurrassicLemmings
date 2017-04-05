8f3aa68f63ca06f654f4280e53f54509a2ad71ce
Benjamin Reid <benjamin@nouveller.com>
2017-04-05 19:35:02 +0100

    Dig and styles

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var express = require('express')
 var app = express()
 var path = require("path");
 
+app.use(express.static('assets'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.get('/dig', function (req, res) {
+  res.sendFile(path.join(__dirname+'/dig.html'));
 })
 
 app.listen(3000, function () {
