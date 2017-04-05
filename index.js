e90a8d9ef098f9a4623133c891978158a013f705
Benjamin Reid <benjamin@nouveller.com>
2017-04-05 20:54:24 +0100

    Merge branch 'master' of github.com:Rumyra/jurrassicLemmings

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
+
 var express = require('express')
 var app = express()
 var path = require("path");
+var Pusher = require('pusher');
+var bodyParser = require('body-parser');
+
+var pusher = new Pusher({
+  appId: '324273',
+  key: '3243046120d5ad58ddaf',
+  secret: '93c1950d6650abedec4a',
+  cluster: 'eu',
+  encrypted: true
+});
+
+app.use( bodyParser.json() );
 
 app.use(express.static('assets'));
 
@@ -10,6 +24,11 @@ app.get('/', function (req, res) {
 
 app.get('/dig', function (req, res) {
   res.sendFile(path.join(__dirname+'/dig.html'));
+})
+
+app.post('/send', function (req, res) {
+  console.log(req.body)
+  // pusher.trigger('my-channel', 'submit', { data: true });
 })
 
 app.listen(3000, function () {
